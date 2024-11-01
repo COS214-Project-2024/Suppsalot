035a8a282ac6e3d09b25a886257ba1baa46245df
u22608789 <u22608789@tuks.co.za>
2024-11-02 00:54:07 +0200

    Removed Tax folder from Transportation branch

--- Tax/TaxHistoryManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "TaxHistoryManager.h"
-
-void TaxHistoryManager::addMemento(const TaxMemento& memento) {
-    history_.push_back(memento);
-}
-
-const std::vector<TaxMemento>& TaxHistoryManager::getHistory() const {
-    return history_;
-}
